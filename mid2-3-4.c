474984d01f69c5f62792cbd669e1b56b001c805c
nzp89 <92073106+nzp89@users.noreply.github.com>
2024-12-09 23:21:28 +0900

    Create mid2-3-4.c

---
+++ mid2-3-4.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <err.h>
+#include <errno.h>
+
+int N = 100000;
+struct timeval t0;
+FILE *fp;
+
+void *thread(void *args)
+{
+    int start = *((int *)args); // 開始番号
+    int end = start + N / 4;    // 終了番号
+    struct timeval t1;
+
+    for (int i = start; i < end; i++)
+    {
+        int sum_yakusu = 0;
+        for (int j = 1; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                sum_yakusu += j;
+            }
+        }
+        if (sum_yakusu == i)
+        {
+            printf("perfect number: %d\n", i);
+        }
+    }
+
+    gettimeofday(&t1, NULL);
+    timersub(&t1, &t0, &t1);
+    fprintf(fp, "sub-thread runtime: %ld.%06ld\n", t1.tv_sec, t1.tv_usec);
+    return NULL;
+}
+
+int main()
+{
+    // 初期化
+    gettimeofday(&t0, NULL);
+    fp = fopen("output.txt", "w");
+
+    // 4つのスレッドを作成
+    pthread_t th1, th2, th3, th4;
+    int start1 = 1, start2 = N / 4, start3 = 2 * N / 4, start4 = 3 * N / 4;
+
+    pthread_create(&th1, NULL, thread, (void *)&start1);
+    pthread_create(&th2, NULL, thread, (void *)&start2);
+    pthread_create(&th3, NULL, thread, (void *)&start3);
+    pthread_create(&th4, NULL, thread, (void *)&start4);
+
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+    pthread_join(th3, NULL);
+    pthread_join(th4, NULL);
+
+    // 終了時間
+    struct timeval t2;
+    gettimeofday(&t2, NULL);
+    timersub(&t2, &t0, &t2);
+    fprintf(fp, "main-thread runtime: %ld.%06ld\n", t2.tv_sec, t2.tv_usec);
+
+    return 0;
+}
